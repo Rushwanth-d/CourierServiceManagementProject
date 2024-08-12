dbd1f7e4540ef7a41a4c79255c5d7238770d3708
Rushwanth-d <91526189+Rushwanth-d@users.noreply.github.com>
2024-08-12 09:19:19 +0530

    Update ContinuousCouriersTests.java

--- ContinuousCourier/src/test/java/com/klef/jfsd/springboot/ContinuousCouriersTests.java
+++ ContinuousCourier/src/test/java/com/klef/jfsd/springboot/ContinuousCouriersTests.java
@@ -1,13 +1,1 @@
-package com.klef.jfsd.springboot;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ContinuousCouriersTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
